791c127f1739c7a54d365165ad3f135428144f8a
sajid507 <sajidmunawar666@gmail.com>
2020-11-05 19:14:21 +0500

    created async await function

---
+++ async_await.js
@@ -0,0 +1,25 @@
+function apiFetch(){
+    let promise=new Promise(function(resolve,reject){
+        setTimeout(function(){
+            console.log("successful")
+            resolve("data")
+            reject()
+
+        },1000)
+    })
+    // .then(function(){
+        // console.log("this is then")
+    // .catch(function(){
+    //     console.log("this is catch")
+    // })
+    // })
+    return promise;
+}
+
+apiFetch();
+async function _asyncFunction(){
+    let aa=await apiFetch()
+    console.log(`${aa} is came from resolve`)
+}
+
+_asyncFunction()
