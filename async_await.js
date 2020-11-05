1a673b8a07e4622bf433473cd0d71785c8a020b2
sajid507 <sajidmunawar666@gmail.com>
2020-11-05 19:17:21 +0500

    Update async_await.js

--- async_await.js
+++ async_await.js
@@ -2,15 +2,15 @@ function apiFetch(){
     let promise=new Promise(function(resolve,reject){
         setTimeout(function(){
             console.log("successful")
-            resolve("data")
+            // resolve("data")
             reject()
 
         },1000)
     })
-    .then(function(){
-        console.log("this is then")
-    // .catch(function(){
-    //     console.log("this is catch")
+    // .then(function(){
+        // console.log("this is then")
+    .catch(function(){
+        console.log("this is catch")
     })
     // })
     return promise;
